c60d4188c644e37f08247123628539764b44df84
Nicolas-Sosa1 <nicolassosaesteban2003@gmail.com>
2025-09-20 22:55:31 -0300

    Agrego clase Seguros.java

---
+++ src/main/java/entidades/Seguros.java
@@ -0,0 +1,66 @@
+package entidades;
+
+public class Seguros {
+	
+	private int idSeguros;
+	private String descripcion;
+	private int idTipo;
+	private double costoContratacion;
+	private double costoAsegurado;
+	
+	public Seguros() {
+		
+	}
+
+	public int getIdSeguros() {
+		return idSeguros;
+	}
+
+	public void setIdSeguros(int idSeguros) {
+		this.idSeguros = idSeguros;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+		
+	}
+
+	public int getIdTipo() {
+		return idTipo;
+	}
+
+	public void setIdTipo(int idTipo) {
+		this.idTipo = idTipo;
+	}
+
+	public double getCostoContratacion() {
+		return costoContratacion;
+	}
+
+	public void setCostoContratacion(double costoContratacion) {
+		this.costoContratacion = costoContratacion;
+	}
+	
+	public double getCostoAsegurado() {
+		return costoAsegurado;
+	}
+
+	public void setCostoAsegurado(double costoAsegurado) {
+		this.costoAsegurado = costoAsegurado;
+	}
+
+	@Override
+	public String toString() {
+		return "Seguros [idSeguros=" + idSeguros + ", descripcion=" + descripcion + ", idTipo=" + idTipo
+				+ ", costoContratacion=" + costoContratacion + "]";
+	}
+	
+	
+	
+	
+
+}
